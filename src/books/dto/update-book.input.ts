5eb7ac4689d0b799e1f1d8fd6f435dc678194000
Santi <yepes.s07@gmail.com>
2026-04-23 11:36:10 -0500

    Fix Update Book

--- src/books/dto/update-book.input.ts
+++ src/books/dto/update-book.input.ts
@@ -1,8 +1,11 @@
 import { CreateBookInput } from './create-book.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { IsInt, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class UpdateBookInput extends PartialType(CreateBookInput) {
   @Field(() => Int)
+  @IsInt()
+  @IsNotEmpty()
   id!: number;
 }
